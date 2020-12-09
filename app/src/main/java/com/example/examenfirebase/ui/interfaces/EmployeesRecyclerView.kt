8c7e993e76a7a5a9dff433411d16f1bab77153b2
Jose Aldair Nava Correa <jose.aldair@bosc.solutions>
2020-12-09 11:26:15 -0600

    recycler

---
+++ app/src/main/java/com/example/examenfirebase/ui/interfaces/EmployeesRecyclerView.kt
@@ -0,0 +1,12 @@
+package com.example.examenfirebase.ui.interfaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.examenfirebase.R
+
+class EmployeesRecyclerView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employees_recycler_view)
+    }
+}
